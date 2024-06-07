654f10695363e7cd746759b26c01840f03b69b2e
sandeepy81 <166781989+sandeepy81@users.noreply.github.com>
2024-06-06 23:44:23 -0400

    Add files via upload

---
+++ operators/arithmeticoperators.py
@@ -0,0 +1,8 @@
+x,y=12,5
+print("Substraction: ",x-y)
+print("Addition: ",x+y)
+print("Div: ",x/y)
+print("Mul: ",x*y)
+print("Mod: ",x%y)
+print("Power: ",x**y)
+print("Floor Div: ",x//y)
---
+++ operators/comparisonoperator.py
@@ -0,0 +1,14 @@
+x,y=77,88
+
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+
+##logical operators
+x=20
+y=30
+print((x==20 and y==30))
+print(x==20 or y==30)
